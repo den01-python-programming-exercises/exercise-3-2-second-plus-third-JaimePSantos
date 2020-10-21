afffd95289c0f6c930b7b750e2f6c62895d6b0ec
Jaime Santos <jaimepereirasantos123@gmail.com>
2020-10-21 20:17:16 +0000

    Its second plus third but test is for first element of list.

--- src/exercise.py
+++ src/exercise.py
@@ -1,5 +1,11 @@
 def main():
-    #write your code below this line
+    intList = []
+    while(True):
+      number = int(input())
+      if(number==0):
+        break
+      intList.append(number)
+    print(intList[0])
 
 if __name__ == '__main__':
     main()
